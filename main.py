f921f1cac2828c353998c6569d25aeb7312e6a1b
Murali Krishna Talari <85298075+MuraliKrishna1005@users.noreply.github.com>
2022-03-20 15:55:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os, sys
-import psycopg2
+import sqlite3
 from flask import Flask, render_template, request
 
-app = Flask(__name__,template_folder=’templates’)
+app = Flask(__name__,template_folder='templates')
 #functions to be added here for
 #different actions
 
-if __name__ == ’__main__’:
+if __name__ == '__main__':
 # Run the Flask app
-app.run(host=’127.0.0.1’,debug=True,port=5000)
+  app.run(host='0.0.0.0',debug=True,port=5000)
 
 @app.route("/")
 def index():
@@ -25,18 +25,17 @@ def saveDetails():
 	email = request.form["email"]
 	conn = None
 	try:
-		conn = psycopg2.connect(database = "mydb", user = "myuser", \
-		password = "mypass", host = "127.0.0.1", port = "5432") # connect to the PostgreSQL database
+		conn = sqlite3.connect('example.db') # connect to the PostgreSQL database
 		cur = conn.cursor() # create a new cursor
 		cur.execute("INSERT INTO Candidate (cno, name, email) \
 		VALUES (%s, %s, %s)", (cno, name, email)) # execute the INSERT statement
 		conn.commit() # commit the changes to the database
 		cur.close() # close the cursor
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, sqlite3.DatabaseError) as error:
 		render_template("fail.html")
 	finally:
 		if conn is not None:
-		conn.close() # close the connection
+		  conn.close() # close the connection
 	return render_template("success.html")
 
 @app.route("/viewall")
@@ -44,14 +43,13 @@ def viewAll():
 	conn = None
 	try:
 		# connect to the PostgreSQL database
-		conn = psycopg2.connect(database = "mydb", user = "myuser", \
-		password = "mypass", host = "127.0.0.1", port = "5432")
+		conn = sqlite3.connect('example.db')
 		cur = conn.cursor() # create a new cursor
 		# execute the SELECT statement
 		cur.execute("SELECT cno, name, email FROM Candidate")
 		results = cur.fetchall() # fetches all rows of the query result set
 		cur.close() # close the cursor
-	except (Exception, psycopg2.DatabaseError) as error:
+	except (Exception, sqlite3.DatabaseError) as error:
 		render_template("fail.html")
 	finally:
 		conn.close() # close the connection
